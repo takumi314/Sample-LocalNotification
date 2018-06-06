096b98eb7f25bc875f437c373c4da342004d637c
takumi314 <isospin_symmetry@me.com>
2018-06-07 01:12:14 +0900

    [feat] implement Local Notification

--- Sample-LocalNotification/AppDelegate.swift
+++ Sample-LocalNotification/AppDelegate.swift
@@ -7,40 +7,86 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    
+    // Call after application launch.
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+
+        // Reset the budge into zero
+        application.applicationIconBadgeNumber = 0
+
+        if #available(iOS 10, *) {
+            // The delegate must be set before the application returns from application:didFinishLaunchingWithOptions:.
+            UNUserNotificationCenter.current().delegate = self
+
+            // 通知の許可を得る
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound ]) {
+                (granted: Bool, error: Error?) in
+                if let e = error {
+                    print(e.localizedDescription)
+                } else if granted {
+                    print("authorized")
+                }
+            }
+
+        } else {
+            application.registerForRemoteNotifications()
+        }
+
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        let badgeNumber = application.applicationIconBadgeNumber
+        let content = UNMutableNotificationContent()
+        content.title = "Title"
+        content.subtitle = "Sub title"
+        content.body = "Body"
+        content.badge = NSNumber(value: badgeNumber + 1)
+        content.sound = UNNotificationSound.default()
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        let request = UNNotificationRequest(identifier: "identifier", content: content, trigger: trigger)
+
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error: Error?) in
+            print(error?.localizedDescription ?? "nil")
+        }
+
     }
 
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    /// The method will be called on the delegate only if the application is in the foreground.
+    /// If the method is not implemented or the handler is not called in a timely manner then the notification will not be presented.
+    ///    (cf. sound, badge, alert and/or in the notification list)
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+
+        print("The application is in the foreground.")
+
+        // Required to present the notification
+        completionHandler([.alert, .badge, .sound])
+
+    }
+
+    /// The method will be called on the delegate when the user responded to the notification by opening the application,
+    /// dismissing the notification or choosing a UNNotificationAction.
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+        print("The user responded to the notification.")
+
+        // Reset the budge
+        UIApplication.shared.applicationIconBadgeNumber = 0
+    }
+
+}
